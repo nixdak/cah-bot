a883ffaa0c8b2f9f6901997ed0bf5a5b86392e60
Craig Gavagan <craig.gavagan@gmail.com>
2015-05-22 23:40:14 +0100

    Adding base for #1

--- db/typo_fixes.js
+++ db/typo_fixes.js
@@ -1,9 +1,20 @@
 var models = require('../models');
 
-var question_fixes = {
+var question_fixes = [
 
-};
+];
 
-var answer_fixes = {
+question_fixes.forEach(function(typo_fix) {
+  models.Question.update({text: typo_fix.correct_text}, {where: {text: wrong_text}});
+});
 
+var answer_fixes = [
+  {
+    wrong_text: 'The suden need to conceal a massive boner',
+    correct_text: 'The sudden need to conceal a massive boner'
+  }
+];
+
+answer_fixes.forEach(function(typo_fix) {
+  models.Question.update({text: typo_fix.correct_text}, {where: {text: wrong_text}});
 };
